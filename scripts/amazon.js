28a7b6cf2e22eb78b5908cffa448f38a171a3a80
abdulsamad <abdulsamad3775@gmail.com>
2023-06-06 13:47:38 +0530

    mark the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -74,6 +74,11 @@ forEach((button)=>{
         quantity:1
       });
     }
-    console.log(cart);
+
+    let cartQuantity=0;
+    cart.forEach((item)=>{
+      cartQuantity+=item.quantity;
+    });
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
    });
 });
